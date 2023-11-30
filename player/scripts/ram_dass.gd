1f3ebe3db77c321f31cf55cb12bcfdb6ad3cdf9d
Guilherme <gui.crb0@gmail.com>
2023-11-29 22:57:32 -0300

    idle animations

--- player/scripts/ram_dass.gd
+++ player/scripts/ram_dass.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
-var speed: int = 50
-
+var speed: int = 75
 enum Direction { NORTH, SOUTH, EAST, WEST }
+var currentDirection: Direction = Direction.SOUTH
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
 
 	if buttonPressed(Direction.NORTH):
 		if not (buttonPressed(Direction.SOUTH) or buttonPressed(Direction.EAST) or buttonPressed(Direction.WEST)):
-			velocity.y = -speed
+			velocity.y = -speed/1.5
+			currentDirection = Direction.NORTH
 	elif buttonPressed(Direction.SOUTH):
 		if not (buttonPressed(Direction.EAST) or buttonPressed(Direction.WEST)):
-			velocity.y = speed
+			velocity.y = speed/1.5
+			currentDirection = Direction.SOUTH
 	elif buttonPressed(Direction.EAST):
 		if not (buttonPressed(Direction.WEST)):
+			currentDirection = Direction.EAST
 			velocity.x = speed
+		turnLeftAnimation(false)
 	elif buttonPressed(Direction.WEST):
 		velocity.x = -speed
+		currentDirection = Direction.WEST
+		turnLeftAnimation(true)
 
 	move_and_slide()
+	
+	#if velocity == Vector2(0,0):
+	if true:
+		var animationKey: String = ""
+		match currentDirection:
+			Direction.NORTH:
+				animationKey = "north"
+			Direction.SOUTH:
+				animationKey = "south"
+			Direction.EAST, Direction.WEST:
+				animationKey = "horizontal"
+		$IdleAnimation.play(animationKey)
 
 func buttonPressed(direction: Direction) -> bool:
 	match direction:
@@ -34,4 +52,7 @@ func buttonPressed(direction: Direction) -> bool:
 
 	# Default case
 	return false
+
+func turnLeftAnimation(flip: bool):
+	$Sprite.flip_h = flip
 
